d2827db2f1e3586b7ca16539e2f5455d7f0d76d3
JCasera <jtc2123@gmail.com>
2022-10-09 11:58:43 -0400

    Add click mission to toggle complete (visual only)

--- Tile.gd
+++ Tile.gd
@@ -2,5 +2,19 @@ extends CenterContainer
 
 onready var mission_text = $Label
 
+var complete = false
+
 func update_text(t = ""):
 	mission_text.text = t
+
+
+func _on_Tile_gui_input(event):
+	# Highlight on Hover
+	# Grey out on click
+	if event is InputEventMouseButton && event.pressed:
+		complete = !complete
+		if complete:
+			mission_text.add_color_override("font_color", Color(0,0,0))
+		else:
+			mission_text.add_color_override("font_color", Color("ffffff"))
+	pass

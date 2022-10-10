20b851482c7f6ff6ab2d506b8c309ebd62afb465
JCasera <jtc2123@gmail.com>
2022-10-09 22:16:03 -0400

    Add highlight to text when hovering over the tile

--- Tile.gd
+++ Tile.gd
@@ -7,14 +7,22 @@ var complete = false
 func update_text(t = ""):
 	mission_text.text = t
 
+func update_text_display():
+	if complete:
+		mission_text.add_color_override("font_color", Color(0,0,0))
+	else:
+		mission_text.add_color_override("font_color", Color("ffffff"))
 
 func _on_Tile_gui_input(event):
-	# Highlight on Hover
 	# Grey out on click
 	if event is InputEventMouseButton && event.pressed:
 		complete = !complete
-		if complete:
-			mission_text.add_color_override("font_color", Color(0,0,0))
-		else:
-			mission_text.add_color_override("font_color", Color("ffffff"))
+		update_text_display()
 	pass
+
+
+func _on_Tile_mouse_entered():
+	mission_text.add_color_override("font_color_shadow", Color("ffffcc00"))
+
+func _on_Tile_mouse_exited():
+	mission_text.add_color_override("font_color_shadow", Color("00ffcc00"))

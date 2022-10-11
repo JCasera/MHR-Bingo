52dcdacd0f87c36800cbc8a4a32eddf56257b2bc
JCasera <jtc2123@gmail.com>
2022-10-11 18:39:45 -0400

    Add seed display and generate with seed to the options

--- Window.gd
+++ Window.gd
@@ -2,15 +2,16 @@ extends VBoxContainer
 
 onready var board = $Board
 onready var options = $Options
+onready var rng_seed = $Options/Seed
 onready var tile = preload("res://Tile.tscn")
 
 var mission_list
 var fill_in_values
+var bingo_options
 const board_size = 25
 
 var rng = RandomNumberGenerator.new()
 var used_values = []
-var used_missions = []
 var regex = RegEx.new()
 
 const rare_life_placeholder = "[rare_endemic_life]"
@@ -25,6 +26,7 @@ func _ready():
 	regex.compile("\\[[a-z_]*\\]")
 	load_monsters()
 	load_missions()
+	load_options()
 
 func load_missions():
 	var f = File.new()
@@ -38,6 +40,12 @@ func load_monsters():
 	fill_in_values = parse_json(f.get_as_text())
 	f.close()
 
+func load_options():
+	var f = File.new()
+	f.open("res://data/options.json", File.READ)
+	bingo_options = parse_json(f.get_as_text())
+	f.close()
+
 func roll_missions():
 	# 2-4 restrictions
 	# 1-2 gathering
@@ -46,35 +54,38 @@ func roll_missions():
 	# 4-6 quest/arena
 	# remaining should be target missions
 	used_values.clear()
-	used_missions.clear()
 	var board_missions = []
+	
+	# Save rng state
+	rng_seed.text = str(rng.state)
+	
 	var restriction_count = rng.randi_range(2,4)
 	var gathering_count = rng.randi_range(1,2)
 	var exploring_count = rng.randi_range(1,2)
 	var battle_count = rng.randi_range(1,2)
 	var quest_count = rng.randi_range(4,6)
 
-	board_missions.append_array(get_missions_for_type("restrictions", restriction_count))
-	board_missions.append_array(get_missions_for_type("gathering", gathering_count))
-	board_missions.append_array(get_missions_for_type("exploring", exploring_count))
+	board_missions.append_array(select_randomized_missions(get_mission_list_for(["restrictions"]), restriction_count))
+	board_missions.append_array(select_randomized_missions(get_mission_list_for(["gathering"]), gathering_count))
+	board_missions.append_array(select_randomized_missions(get_mission_list_for(["exploring"]), battle_count))
 #	board_missions.append_array(get_missions_for_type("battle", battle_count))
 #	board_missions.append_array(get_missions_for_type("quest", quest_count))
-	board_missions.append_array(get_missions_for_type("target", board_size - board_missions.size()))
+	board_missions.append_array(select_randomized_missions(get_mission_list_for(["target", "hunts"]), board_size - board_missions.size()))
 
 	return board_missions
 
-func get_missions_for_type(type, count):
+func select_randomized_missions(list, count):
 	var board_missions = []
 	for i in count:
 		var m = null
 		while m == null:
-			m = get_mission(type, rng.randi_range(0, get_mission_count_by_type(type)-1))
+			m = list[rng.randi_range(0, list.size()-1)]
 			var placeholder = regex.search(m)
 			if placeholder != null:
 				m = replace_placeholder(m, placeholder.get_string())
 			if board_missions.has(m):
 				m = null
-			
+
 		board_missions.append(m)
 	return board_missions
 
@@ -94,15 +105,12 @@ func replace_placeholder(text, tag):
 	used_values.append(replacement)
 	return new_text
 
-func get_mission_count_by_type(mission_type):
-	return mission_list[mission_type].size()
+func get_mission_list_for(types):
+	var list = []
+	for i in types:
+		list.append_array(mission_list[i])
+	return list
 
-func get_mission(type, pos):
-	return mission_list[type][pos]
-
-
-func _on_HowTo_pressed():
-	pass # Replace with function body.
 
 func _on_Generate_pressed():
 	for i in board.get_children():
@@ -122,3 +130,12 @@ func _on_Generate_pressed():
 		board.add_child(l)
 		l.update_text(mission_text)
 
+func _on_Options_pressed():
+	print(bingo_options.keys())
+	print(bingo_options["Generation"].keys())
+	$PopupMenu.popup_centered()
+
+
+func _on_SeededGenerate_pressed():
+	rng.state = int($PopupMenu/OptionList/SharedSeed/CustomSeed.text)
+	_on_Generate_pressed()
